917ec319074a52ea0041613ba141c6c3e707ef03
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2022-08-16 01:52:44 +0300

    Keyboard

--- static/main.js
+++ static/main.js
@@ -4,10 +4,13 @@ const num_cols = 5;
 let current_row = 0;
 let current_col = 0;
 let finished = false;
-let field = document.getElementById("field");
+let key_rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
 let params = new URLSearchParams(window.location.search);
 let mode = params.get("mode") || "random"
 let ws = new WebSocket(`ws://${location.hostname}:${location.port}/${mode}`);
+
+let field = document.getElementById("field");
+let keyboard = document.getElementById("keyboard");
 
 function isLetter(str) {
     return str.length === 1 && ((str >= 'a' && str <= 'z') || (str >= 'A' && str <= 'Z'))
@@ -17,7 +20,7 @@ function initField() {
     field.innerHTML = "";
     for (let i = 0; i < num_rows; ++i) {
         let row = document.createElement("div");
-        row.classList.add("row");
+        row.classList.add("field-row");
         for (let j = 0; j < num_cols; ++j) {
             let letterbox = document.createElement("div");
             letterbox.classList.add("letterbox");
@@ -32,35 +35,95 @@ function getLetterBox(row, col) {
     return field.children[row].children[col];
 }
 
-initField();
-
-window.onkeydown = (event) => {
-    if (finished) {
-        return;
-    }
-    if (isLetter(event.key) && current_row < num_rows && current_col < num_cols) {
+function onLetter(letter) {
+    if (!finished && current_row < num_rows && current_col < num_cols) {
         let letterbox = getLetterBox(current_row, current_col);
         letterbox.classList.remove("blank");
         letterbox.classList.add("focus");
-        letterbox.innerHTML = event.key.toUpperCase();
+        letterbox.innerHTML = letter.toUpperCase();
         ++current_col;
-    } else if (event.key === "Backspace" && current_row < num_rows && current_col > 0) {
+    }
+}
+
+function onBackspace() {
+    if (!finished && current_row < num_rows && current_col > 0) {
         let letterbox = getLetterBox(current_row, current_col - 1);
         letterbox.classList.remove("focus");
         letterbox.classList.add("blank");
         letterbox.innerHTML = "";
         --current_col;
-    } else if (event.key === "Enter" && current_col === num_cols) {
+    }
+}
+
+function onEnter() {
+    if (!finished && current_col === num_cols) {
         let word = "";
         for (let j = 0; j < num_cols; ++j) {
             word += getLetterBox(current_row, j).innerHTML.toLowerCase();
         }
         ws.send(word);
     }
+}
+
+function initKeyboard() {
+    keyboard.innerHTML = "";
+    let backspace_key = document.createElement("button");
+    backspace_key.classList.add("key");
+    backspace_key.classList.add("control");
+    backspace_key.innerHTML = "<i class=\"fas fa-backspace\"></i>";
+    backspace_key.onclick = onBackspace;
+    let enter_key = document.createElement("button");
+    enter_key.classList.add("key");
+    enter_key.classList.add("control");
+    enter_key.innerHTML = "<i class=\"fas fa-arrow-right\"></i>";
+    enter_key.onclick = onEnter;
+    for (let row_id = 0; row_id < key_rows.length; ++row_id) {
+        let row = document.createElement("div");
+        row.classList.add("keyboard-row");
+        if (row_id === 2) {
+            row.appendChild(backspace_key);
+        }
+        for (let c of key_rows[row_id]) {
+            let key = document.createElement("button");
+            key.classList.add("key");
+            key.classList.add("blank");
+            key.innerHTML = c.toUpperCase();
+            key.onclick = () => onLetter(c);
+            row.appendChild(key);
+        }
+        if (row_id === 2) {
+            row.appendChild(enter_key);
+        }
+        keyboard.appendChild(row);
+    }
+}
+
+function getKey(letter) {
+    for (let row_id = 0; row_id < key_rows.length; ++row_id) {
+        for (let col_id = 0; col_id < key_rows[row_id].length; ++col_id) {
+            if (key_rows[row_id][col_id].toUpperCase() === letter) {
+                if (row_id === 2) ++col_id;
+                return keyboard.children[row_id].children[col_id];
+            }
+        }
+    }
+}
+
+initField();
+initKeyboard();
+
+window.onkeydown = (event) => {
+    if (isLetter(event.key)) {
+        onLetter(event.key);
+    } else if (event.key === "Backspace") {
+        onBackspace();
+    } else if (event.key === "Enter") {
+        onEnter();
+    }
 };
 
-ws.onerror = (event) => {
-    window.alert("Unable to connect to server");
+ws.onerror = () => {
+    window.alert("Unable to connect to the server");
 }
 
 ws.onmessage = (event) => {
@@ -77,14 +140,22 @@ ws.onmessage = (event) => {
     }
     for (let j = 0; j < num_cols; ++j) {
         let letterbox = getLetterBox(current_row, j);
+        let key = getKey(letterbox.innerHTML);
         setTimeout(() => {
             letterbox.classList.remove("focus");
             if (message[j] === '.') {
                 letterbox.classList.add("absent");
+                if (key.className === "key blank") {
+                    key.className = "key absent";
+                }
             } else if (message[j] === 'y') {
                 letterbox.classList.add("present");
+                if (key.className === "key blank" || key.className === "key absent") {
+                    key.className = "key present";
+                }
             } else if (message[j] === 'g') {
                 letterbox.classList.add("correct");
+                key.className = "key correct";
             }
         }, 300 * j);
     }
